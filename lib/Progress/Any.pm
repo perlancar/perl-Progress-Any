add8f95ec9fc4ce8570d64cb45848f924c877d31
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-05-16 20:36:26 +0700

    Hackety hack (fill_template() not yet implemented), add some tests

--- lib/Progress/Any.pm
+++ lib/Progress/Any.pm
@@ -4,7 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 
-use Progress::Any::Output::Null;
+use Time::HiRes qw(time);
 
 # VERSION
 
@@ -13,7 +13,7 @@ sub import {
     my $caller = caller();
     for (@args) {
         if ($_ eq '$progress') {
-            my $progress = $self->get_indicator(task => 'main', _init=>0);
+            my $progress = $self->get_indicator(task => 'main');
             {
                 no strict 'refs';
                 my $v = "$caller\::progress";
@@ -25,119 +25,188 @@ sub import {
     }
 }
 
-my %indicators; # key = task
-my $default_output = Progress::Any::Output::Null->new;
+our %indicators; # key = task
+our %outputs;    # key = task, value = [$outputobj, ...]
+
+# attributes of indicator:
+# - target (float)
+# - pos (float*)
+# - finished (bool)
+# - ctime (float*) = creation time
+# - subtargets (float) = (computed) total of subtasks' targets (incl indirect)
+# - luelapseds (array of float*) = elapsed time since last updates (for calculating eta)
+# - luincs (array of float*) = pos increment of last updates (ditto)
 
 sub get_indicator {
     my ($class, %args) = @_;
 
-    my $task   = delete($args{task}) // "main";
-    my $target = delete($args{target});
-    my $output = delete($args{output});
-    my $init   = delete($args{_init}) // 1;
-    die "Unknown argument(s): ".join(", ", keys(%args)) if keys(%args);
+    my $task   = delete($args{task});
+    if (!defined($task)) {
+        my @caller = caller(0);
+        $task = $caller[3] eq '(eval)' ? 'main' : $caller[3];
+        $task =~ s/::/./g;
+    }
+    die "Invalid task syntax '$task'" unless $task =~ /\A\w+(\.\w+)*\z/;
+    my $target   = delete($args{target});
+    my $pos      = delete($args{pos}) // 0;
+    my $finished = delete($args{finished});
+    die "Unknown argument(s) to get_indicator(): ".join(", ", keys(%args))
+        if keys(%args);
     if (!$indicators{$task}) {
-        $indicators{$task} = bless({task=>$task}, $class);
-        $indicators{$task}->init(target=>$target, output=>$output)
-            if $init;
+        $indicators{$task} = bless({
+            task     => $task,
+            ctime    => time(),
+            finished => $finished,
+            target   => $target,
+            pos      => $pos,
+        }, $class);
+
+        # automatically initialize/update parent tasks
+        my $partask = $task;
+        while (1) {
+            last unless $partask =~ s/\.\w+\z//;
+            if (!$indicators{$partask}) {
+                $indicators{$partask} = bless({
+                    task     => $partask,
+                    pos      => 0,
+                    target   => 0,
+                    ctime    => time(),
+                }, $class);
+            }
+            if (defined $target) {
+                if (defined $indicators{$partask}{target}) {
+                    $indicators{$partask}{ctarget} += $target;
+                }
+            } else {
+                $indicators{$partask}{ctarget} = undef;
+            }
+            $indicators{$partask}{pos} += $pos;
+        }
     }
     $indicators{$task};
 }
 
-sub set_output {
-    my ($class, %args) = @_;
-    my $output = $args{output} or die "Please specify output";
+sub target {
+    my $self = shift;
+    if (@_) {
+        my $oldtarget = $self->{target};
+        my $target    = shift;
+        $self->{target} = $target;
 
-    if (my $task = $args{task}) {
-        if ($indicators{$task}) {
-            $indicators{$task}{output} = $output;
-        } else {
-            die "Unknown indicator '$task'";
+        # update parents
+        my $partask = $self->{task};
+        while (1) {
+            $partask =~ s/\.\w+\z// or last;
+            if (defined $target) {
+                if (defined $oldtarget) {
+                    $indicators{$partask}{ctarget} += $target-$oldtarget;
+                } else {
+                    # target becomes defined from undef, we need to recalculate
+                    $indicators{$partask}{ctarget} = 0;
+                  RECOUNT:
+                    for (keys %indicators) {
+                        next unless /\Q$partask\E\.\w+\z/;
+                        if (!defined($indicators{$_}{target})) {
+                            $indicators{$partask}{ctarget} = undef;
+                            last RECOUNT;
+                        } else {
+                            $indicators{$partask}{ctarget} +=
+                                $indicators{$_}{target} +
+                                    ($indicators{$_}{ctarget} // 0);
+                        }
+                    }
+                }
+            } else {
+                $indicators{$partask}{ctarget} = undef;
+            }
         }
+
+        return $self;
     } else {
-        $default_output = $output;
+        return $self->{target};
     }
 }
 
-sub init {
-    my ($self, %args) = @_;
-    die "Already initialized" if $self->{_init}++;
+sub pos {
+    my $self = shift;
+    if (@_) {
+        my $oldpos = $self->{pos};
+        my $pos    = shift;
+        $self->{pos} = $pos;
 
-    $self->{task}   = $args{task} if !defined($self->{task});
-    $self->{target} = $args{target};
-    $self->{output} = $args{output};
-    $self->{pos}    = 0;
-}
+        # update parents
+        my $partask = $self->{task};
+        while (1) {
+            $partask =~ s/\.\w+\z// or last;
+            $indicators{$partask}{pos} += $pos-$oldpos;
+        }
 
-sub set_target {
-    my ($self, %args) = @_;
-    $self->{target} = $args{target};
-    $self->update(pos => $self->{pos});
+        return $self;
+    } else {
+        return $self->{pos};
+    }
 }
 
 sub update {
     my ($self, %args) = @_;
+
+    my $time = time();
+    my $oldpos = $self->{pos};
+
+    # check arguments
     if (defined(my $pos = delete($args{pos}))) {
         $self->{pos} = $pos;
     } else {
-        my $inc = delete($args{inc}) // 1;
-        $self->{pos} += $inc;
+        $self->{pos} += 1;
     }
     $self->{pos} = 0 if $self->{pos} < 0;
     $self->{pos} = $self->{target} if
         defined($self->{target}) && $self->{pos} > $self->{target};
-
     my $message = delete($args{message});
     my $level   = delete($args{level});
     my $status  = delete($args{status});
-    die "Unknown argument(s): ".join(", ", keys(%args)) if keys(%args);
+    die "Unknown argument(s) to update(): ".join(", ", keys(%args))
+        if keys(%args);
 
-    # find output and call it
-    my $output;
-    my $task = $self->{task};
-    my $ind = $self;
-    while (1) {
-        $output = $ind->{output} if $ind;
-        last if defined $output;
-        $task =~ s/\.[^.]+\z// or last;
-        $ind = $indicators{$task};
+    # record times/increments
+    my $inc = $self->{pos} - $oldpos;
+    push @{ $self->{luelapseds} },
+        $self->{lutime} ? $time-$self->{lutime} : $time-$self->{ctime};
+    push @{ $self->{luincs} }, $inc;
+    if (@{ $self->{luincs} } > 5) {
+        shift @{$self->{luelapseds}};
+        shift @{$self->{luincs}};
     }
-    $output //= $default_output;
 
-    my %uargs = (target=>$self->{target}, pos=>$self->{pos},
-                 message=>$message, level=>$level, status=>$status);
-    for (ref($output) eq 'ARRAY' ? @$output : ($output)) {
-        $output->update(%uargs);
+    # update parents' pos
+    if ($self->{pos} != $oldpos) {
+        #
     }
-}
 
-sub reset {
-    my ($self, %args) = @_;
-    $args{message} //= "Reset";
-    $args{pos} = 0;
-    $self->update(%args);
+    # find output(s) and call it
+    {
+        my $task = $self->{task};
+        while (1) {
+            next unless $outputs{$task};
+            for my $output (@{ $outputs{$task} }) {
+                $output->update(
+                    indicator => $indicators{$task},
+                    message   => $message,
+                    level     => $level,
+                    status    => $status,
+                );
+            }
+            last unless $task =~ s/\.\w+\z//;
+        }
+    }
+
+    $self->{lutime} = $time;
 }
 
 sub finish {
     my ($self, %args) = @_;
-    if (defined $self->{target}) {
-        $args{message} //= "Finish";
-        $args{pos} = $self->{target};
-        $self->update(%args);
-    }
+    $self->update(pos=>$self->{target}, finished=>1, %args);
 }
-
-#sub _increment {
-#    my ($self, $inc) = @_;
-#    $inc //= 1;
-#    $self->update(inc=>$inc);
-#}
-#
-#sub _decrement {
-#    my ($self, $dec) = @_;
-#    $dec //= 1;
-#    $self->update(inc=>-$dec);
-#}
 
 1;
 # ABSTRACT: Record progress to any output
@@ -185,7 +254,7 @@ Another example, demonstrating multiple indicators and the LogAny output:
  use Progress::Any::Output;
  use Log::Any::App;
 
- Progress::Any::Output->set('LogAny');
+ Progress::Any::Output->set('LogAny', format => '[%c/%C] %m');
  my $p1 = Progress::Any->get_indicator(task => 'main.download');
  my $p2 = Progress::Any->get_indicator(task => 'main.copy');
 
@@ -234,27 +303,28 @@ The list of features:
 
 You can use different indicator for each task/subtask.
 
-=item * hierarchiecal progress
-
-A task can be divided into subtasks. After a subtask finishes, its parent task's
-progress is incremented by 1 automatically (and if I<that> task is finished,
-I<its> parent is updated, and so on).
-
 =item * customizable output
 
-Output is handled by one of C<Progress::Any::Output::*> modules. Each indicator
-can use one or more outputs. Currently available outputs: null, terminal, log
-(to L<Log::Any>), callback. Other possible output ideas: IM/twitter/SMS, GUI,
-web/AJAX, remote/RPC (over L<Riap> for example, so that
-L<Perinci::CmdLine>-based command-line clients can display progress update from
-remote functions).
+Output is handled by one of C<Progress::Any::Output::*> modules. Currently
+available outputs: C<Null>, C<TermProgressBarColor> (display to terminal as
+progress bar), C<LogAny> (logging using L<Log::Any>), C<Callback>. Other
+possible output ideas: IM/Twitter/SMS, GUI, web/AJAX, remote/RPC (over L<Riap>
+for example, so that L<Perinci::CmdLine>-based command-line clients can display
+progress update from remote functions).
+
+=item * multiple outputs
+
+One or more outputs can be used to display one or more indicators.
+
+=item * hierarchiecal progress
+
+A task can be divided into subtasks. If a subtask is updated, its parent task
+(and its parent, and so on) are also updated proportionally.
 
 =item * message
 
 Aside from setting a number/percentage, allow including a message when updating
 indicator.
-
-=item * indicator reset
 
 =item * undefined target
 
@@ -281,7 +351,7 @@ The main indicator. Equivalent to:
 
 =head2 Progress::Any->get_indicator(%args) => OBJ
 
-Get a progress indicator.
+Get a progress indicator for a certain task.
 
 Arguments:
 
@@ -289,8 +359,21 @@ Arguments:
 
 =item * task => STR (default: main)
 
-If not specified will be set to caller's package + subroutine, e.g. if you are
-calling this method from C<main::foo>, then task will be set to C<main.foo>.
+If not specified will be set to caller's package + subroutine (C<::> will be
+replaced with C<.>), e.g. if you are calling this method from C<main::foo>, then
+task will be set to C<main.foo>. If caller is code inside eval, C<main> will be
+used instead.
+
+=item * target => NUM (default: undef)
+
+Optional. Can be used to initialize target. Will only be done once for the same
+task and not for the subsequent get_indicator() calls on the same task.
+
+=item * pos => NUM (default: 0)
+
+Optional. Can be used to initialize starting position. Will only be done once
+for the same task and not for the subsequent get_indicator() calls on the same
+task.
 
 =back
 
@@ -300,13 +383,11 @@ Get or (re)set target. Can be left or set to undef.
 
 =head2 $progress->pos([ NUM ]) => NUM
 
-Get or set current position.
+Get or set the current position.
 
 =head2 $progress->update(%args)
 
-Update indicator. Will optionally update each associated output(s) if necessary.
-By necessary it means if update maximum frequency and other output's settings
-are not violated.
+Update indicator. Will also, usually, update associated output(s) if necessary.
 
 Arguments:
 
@@ -318,53 +399,94 @@ Set the new position. If unspecified, defaults to current position + 1. If pos
 is larger than target, outputs will generally still show 100%. Note that
 fractions are allowed.
 
-=item * inc => NUM
-
-If C<pos> is not specified, this parameter is used instead, to increment the
-current position by a certain number (the default is 1). Note that fractions are
-allowed.
-
 =item * message => STR
 
 Set a message to be displayed when updating indicator.
 
-=item * level => STR
+=item * level => NUM
 
-EXPERIMENTAL. Setting the importance level of this update. Default is C<normal>
-(or C<low> for fractional update), but can be set to C<high> or C<low>. Output
-can choose to ignore updates lower than a certain level.
+EXPERIMENTAL, NOT YET IMPLEMENTED BY MOST OUTPUTS. Setting the importance level
+of this update. Default is C<normal> (or C<low> for fractional update), but can
+be set to C<high> or C<low>. Output can choose to ignore updates lower than a
+certain level.
 
 =item * status => STR
 
-Set the status of this update. Some outputs interpret/display this, for example
-the Console:
+Set the status of this update, usually done on finish(). Some outputs
+interpret/display this, for example the C<TermMessage> output:
 
 Update:
 
- $progress->update(pos=>2, message=>'Copying file ...');
+ my $progress = Progress::Any->get_indicator(
+     task => 'copy', message => 'Copying file ...');
+ $progress->update(message=>'file1.txt');
 
-Output (C<_> indicates cursor position):
+Output:
 
- Copying file ... _
+ Copying file ... file1.txt
 
 Update:
 
- $progress->update(pos=>2, status=>'success');
+ $progress->finish(status=>'success');
 
 Output:
 
  Copying file ... success
- _
+
+=item * finished => BOOL
+
+Can be set to 1 (e.g. by finish()) if task is completed.
 
 =back
 
 =head2 $progress->finish(%args)
 
-Set indicator to 100% and update output. Equivalent to:
+Equivalent to:
 
- $progress->update(pos => $progress->target, %args);
+ $progress->update(pos => $progress->target, finished=>1, %args);
 
-except that it will still display 100% even though target is unknown/undef.
+The C<< finished => 1 >> part currently does nothing particularly special,
+except to record completion.
+
+=head2 $progress->fill_template($template, \%values)
+
+Fill template with values, like in sprintf. Usually used by output modules.
+Available templates:
+
+=over
+
+=item * C<%a>
+
+Elapsed (absolute) time. Time format is determined by the C<time_format>
+attribute.
+
+=item * C<%e>
+
+Estimated completion time. Time format is determined by the C<time_format>
+attribute.
+
+=item * C<%(width).(prec)p>
+
+Percentage of completion. You can also specify width and precision, like C<%f>
+in Perl sprintf.
+
+=item * C<%(width).(prec)c>
+
+Current position (pos).
+
+=item * C<%(width).(prec)C>
+
+Target.
+
+=item * C<%m>
+
+Message.
+
+=item * C<%%>
+
+A literal C<%>.
+
+=back
 
 
 =head1 SEE ALSO
---
+++ t/01-basics.t
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Test::More 0.98;
+use Progress::Any;
+
+%Progress::Any::indicators = ();
+
+subtest "get_indicator" => sub {
+    Progress::Any->get_indicator(task=>"a.b", target=>10);
+    Progress::Any->get_indicator(task=>"a.b.d", target=>7, pos=>2);
+    Progress::Any->get_indicator(task=>"a.c", target=>5, pos=>1);
+
+    is($Progress::Any::indicators{"a"}{target}, 0, "a's target");
+    is($Progress::Any::indicators{"a"}{ctarget}, 22, "a's ctarget");
+    is($Progress::Any::indicators{"a"}{pos}, 3, "a's pos");
+    is($Progress::Any::indicators{"a.b"}{ctarget}, 7, "a.b's ctarget");
+    is($Progress::Any::indicators{"a.b"}{pos}, 2, "a.b's pos");
+
+    Progress::Any->get_indicator(task=>"a.b.e");
+
+    ok(!defined($Progress::Any::indicators{"a"}{ctarget}), "a's ctarget becomes undefined");
+    ok(!defined($Progress::Any::indicators{"a.b"}{ctarget}), "a.b's ctarget becomes undefined");
+};
+
+subtest "target" => sub {
+    my $p = Progress::Any->get_indicator(task=>"a.b.e");
+
+    # stays undef
+    $p->target(undef);
+
+    ok(!defined($Progress::Any::indicators{"a"}{ctarget}), "a's ctarget stays undefined");
+    ok(!defined($Progress::Any::indicators{"a.b"}{ctarget}), "a.b's ctarget stays undefined");
+
+    # undef becomes defined
+    $p->target(3);
+
+    is($Progress::Any::indicators{"a"}{ctarget}, 25, "a's ctarget becomes defined");
+    is($Progress::Any::indicators{"a.b"}{ctarget}, 10, "a.b's ctarget becomes defined");
+
+    # defined stays defined
+    $p->target(5);
+
+    is($Progress::Any::indicators{"a"}{ctarget}, 27, "a's ctarget updated");
+    is($Progress::Any::indicators{"a.b"}{ctarget}, 12, "a.b's ctarget updated");
+
+    # defined becomes undef
+    $p->target(undef);
+
+    ok(!defined($Progress::Any::indicators{"a"}{ctarget}), "a's ctarget becomes undefined");
+    ok(!defined($Progress::Any::indicators{"a.b"}{ctarget}), "a.b's ctarget becomes undefined");
+};
+
+subtest "pos" => sub {
+    my $p = Progress::Any->get_indicator(task=>"a.b.e");
+
+    $p->pos(1);
+
+    is($Progress::Any::indicators{"a"}{pos}, 4, "a's pos updated");
+    is($Progress::Any::indicators{"a.b"}{pos}, 3, "a.b's pos updated");
+
+    $p->pos(3);
+
+    is($Progress::Any::indicators{"a"}{pos}, 6, "a's pos updated (2)");
+    is($Progress::Any::indicators{"a.b"}{pos}, 5, "a.b's pos updated (2)");
+};
+
+#subtest "update" => sub {
+#};
+
+DONE_TESTING:
+done_testing;
